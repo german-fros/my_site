cd6a97fb861b10b4331cecb0df1a7b4fb139ec0a
German Fros <german.fros551@gmail.com>
2023-08-10 16:54:49 -0300

    exercicio

---
+++ mysite/tests/factories.py
@@ -0,0 +1,36 @@
+import factory
+from Faker import Factory as FakerFactory
+
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+from my_site.mysite.blog.models import Post
+
+faker = FakerFactory.create()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    email = factory.Faker('safe_email')
+    username = factory.LazyAttribute(lambda x: faker.name())
+
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        password = kwargs.pop("password", None)
+        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        if password:
+            user.set_password(password)
+            if create:
+                user.save()
+        return user
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    title = factory.LazyAttribute(lambda x: faker.sentence())
+    create_on = factory.LazyAttribute(lambda x: now())
+    author = factory.SubFactory(UserFactory)
+    status = 0
+
+    class Meta:
+        model = Post
